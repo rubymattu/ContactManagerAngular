982f4751ea411baf9e14a9cbc55726523c2dc11b
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-06-25 09:10:12 -0400

    Added connect.php

---
+++ contactapi/connect.php
@@ -0,0 +1,22 @@
+<?php
+
+  // Database connection settings
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'contact_manager');
+
+  // Create a connection to the database
+  function connect() {
+      $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+      // Check if the connection was successful
+      if(mysqli_connect_errno()) {
+          die("Connection failed: " . mysqli_connect_error());
+      }
+      mysqli_set_charset($connect, "utf8");
+      return $connect;
+  }
+  $con = connect();
+
+?>
