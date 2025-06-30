ba582c2e088f7d0c7ae3d5ecae3e3e8e83aafd81
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-06-30 08:43:58 -0400

    Added Routing

--- contactmanager/src/app/app.routes.ts
+++ contactmanager/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Contact } from './contact/contact';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "contact", component: Contact },
+  { path: "about", component: About },
+  { path: "", redirectTo: "/contact", pathMatch: "full" },
+];
--- contactmanager/src/app/app.ts
+++ contactmanager/src/app/app.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgForm } from '@angular/forms';
+
+import { Contact } from './contact';
+import { ContactService } from './contact.service';
+
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
