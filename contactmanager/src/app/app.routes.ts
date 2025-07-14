0c55b50282d5c120ff975918864ccf58a64d32b5
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-07-14 08:45:57 -0400

    Routing edit

--- contactmanager/src/app/app.routes.ts
+++ contactmanager/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Contacts } from './contacts/contacts';
 import { Addcontacts } from './addcontacts/addcontacts';
+import { Updatecontacts } from './updatecontacts/updatecontacts';
 
 export const routes: Routes = [
     { path: "contacts", component: Contacts},
     { path: "add", component: Addcontacts},
     { path: "about", component: About},
+    { path: "edit/:id", component: Updatecontacts},
     { path: "**", redirectTo: "/contacts"}
 ];
---
+++ contactmanager/src/app/updatecontacts/updatecontacts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updatecontacts',
+  imports: [],
+  templateUrl: './updatecontacts.html',
+  styleUrl: './updatecontacts.css'
+})
+export class Updatecontacts {
+
+}
